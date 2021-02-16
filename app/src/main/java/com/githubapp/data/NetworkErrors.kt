d93578302c97cb4ae37719cb2dbe4e5d3f944bcb
darko <darko.grujic@quantox.com>
2021-02-16 19:44:45 +0100

    Fixed bug with api call class Removed unused val

--- app/src/main/java/com/githubapp/data/NetworkErrors.kt
+++ app/src/main/java/com/githubapp/data/NetworkErrors.kt
@@ -3,12 +3,10 @@ package com.githubapp.data
 object NetworkErrors {
 
     private const val UNABLE_TO_RESOLVE_HOST = "Unable to resolve host"
-    const val UNABLE_TODO_OPERATION_WO_INTERNET = "Can't do that operation without an internet connection"
-    const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
+    const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection"
     const val NETWORK_ERROR_UNKNOWN = "Unknown network error"
     const val NETWORK_ERROR = "Network error"
     const val NETWORK_ERROR_TIMEOUT = "Network timeout"
-    const val NETWORK_DATA_NULL = "Network data is null"
     const val ERROR_UNKNOWN = "Unknown error"
 
 
--- app/src/main/java/com/githubapp/data/RepositoryExtensions.kt
+++ app/src/main/java/com/githubapp/data/RepositoryExtensions.kt
@@ -2,8 +2,10 @@ package com.githubapp.data
 
 import com.githubapp.data.NetworkErrors.ERROR_CHECK_NETWORK_CONNECTION
 import com.githubapp.data.NetworkErrors.ERROR_UNKNOWN
+import com.githubapp.data.NetworkErrors.NETWORK_ERROR
 import com.githubapp.data.NetworkErrors.NETWORK_ERROR_TIMEOUT
 import com.githubapp.data.NetworkErrors.NETWORK_ERROR_UNKNOWN
+import com.githubapp.data.NetworkErrors.isNetworkError
 import com.githubapp.util.Constants.NETWORK_TIMEOUT
 import com.githubapp.util.Resource
 import kotlinx.coroutines.CoroutineDispatcher
@@ -13,7 +15,6 @@ import kotlinx.coroutines.withTimeout
 import retrofit2.HttpException
 import timber.log.Timber
 import java.io.IOException
-import java.net.UnknownHostException
 
 suspend fun <T> apiCall(
     dispatcher: CoroutineDispatcher,
@@ -28,26 +29,23 @@ suspend fun <T> apiCall(
         throwable.printStackTrace()
         when (throwable) {
             is TimeoutCancellationException -> {
-                Timber.e(NETWORK_ERROR_TIMEOUT)
                 Resource.Error(NETWORK_ERROR_TIMEOUT)
-            }
-            is UnknownHostException -> {
-                Timber.e(ERROR_CHECK_NETWORK_CONNECTION)
-                Resource.Error(ERROR_CHECK_NETWORK_CONNECTION)
             }
             is IOException -> {
-                Timber.e(NETWORK_ERROR_TIMEOUT)
-                Resource.Error(NETWORK_ERROR_TIMEOUT)
+                throwable.localizedMessage?.let { message ->
+                    if (isNetworkError(message)) {
+                        Timber.e(ERROR_CHECK_NETWORK_CONNECTION)
+                        return@withContext Resource.Error(ERROR_CHECK_NETWORK_CONNECTION)
+                    }
+                }
+                Resource.Error(NETWORK_ERROR)
             }
             is HttpException -> {
                 val errorResponse = convertErrorBody(throwable)
                 Timber.e(errorResponse)
-                Resource.Error(
-                    errorResponse!!
-                )
+                Resource.Error(errorResponse ?: ERROR_UNKNOWN)
             }
             else -> {
-                Timber.e(NETWORK_ERROR_UNKNOWN)
                 Resource.Error(NETWORK_ERROR_UNKNOWN)
             }
         }
@@ -56,10 +54,8 @@ suspend fun <T> apiCall(
 
 private fun convertErrorBody(throwable: HttpException): String? {
     return try {
-        Timber.e(throwable.localizedMessage)
         throwable.localizedMessage
     } catch (exception: Exception) {
-        Timber.e(ERROR_UNKNOWN)
         ERROR_UNKNOWN
     }
 }
